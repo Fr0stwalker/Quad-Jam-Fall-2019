ec8214edd1473259cce9d5ae0b099a8973f0a163
Ikuti <m.j.olkiewicz@gmail.com>
2019-10-26 17:59:07 +0200

    Reworked movement, works good now!

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,31 +16,34 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!stop)
-        {
-            transform.Translate(Time.deltaTime * speed * Vector3.forward);
-            if (transform.eulerAngles.y == 0 || transform.eulerAngles.y == 180)
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                if (Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    transform.eulerAngles = new Vector3(0, 0, 0);
-                }
-                else if (Input.GetKeyDown(KeyCode.LeftArrow))
-                {
-                    transform.eulerAngles = new Vector3(0, 180, 0);
-                }
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    transform.eulerAngles = new Vector3(0, 270, 0);
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    transform.eulerAngles = new Vector3(0, 90, 0);
-                }
+                transform.eulerAngles = new Vector3(0, 180, 0);
             }
-        }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.eulerAngles = new Vector3(0, 270, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.eulerAngles = new Vector3(0, 90, 0);
+            }
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), 1f))
+            {
+                transform.position=new Vector3(Mathf.Round(transform.position.x),0,Mathf.Round(transform.position.z));
+                stop = true;
+            }
+            else if (stop)
+            {
+                stop = false;
+            }
+            if (!stop)
+            {
+                transform.Translate(Time.deltaTime * speed * Vector3.forward);
+            }
     }
 }
