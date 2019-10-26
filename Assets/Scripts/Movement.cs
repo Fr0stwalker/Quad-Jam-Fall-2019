859722edbb04ecfdd62e12e568ae68c2c21d6452
Ikuti <m.j.olkiewicz@gmail.com>
2019-10-26 19:08:58 +0200

    Changed up movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,25 +16,24 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetKeyDown(KeyCode.RightArrow) && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), .5f))
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y+90f, 0);
             }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), .5f))
             {
-                transform.eulerAngles = new Vector3(0, 180, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y - 90f, 0);
+        }
+            //if (Input.GetKeyDown(KeyCode.UpArrow))
+            //{
+            //    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180f, 0);
+            //}
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), .5f))
             {
-                transform.eulerAngles = new Vector3(0, 270, 0);
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y -180f, 0);
+        }
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), .5f))
             {
-                transform.eulerAngles = new Vector3(0, 90, 0);
-            }
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), 1f))
-            {
-                transform.position=new Vector3(Mathf.Round(transform.position.x),0,Mathf.Round(transform.position.z));
                 stop = true;
             }
             else if (stop)
@@ -44,6 +43,7 @@ public class Movement : MonoBehaviour
             if (!stop)
             {
                 transform.Translate(Time.deltaTime * speed * Vector3.forward);
+                //transform.position = new Vector3(Mathf.Round(transform.position.x), 0, Mathf.Round(transform.position.z));
             }
     }
 }
