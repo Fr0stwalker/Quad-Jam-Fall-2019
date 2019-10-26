54ec37b1d6eed9d452af678698972cfae7d97f75
Ikuti <m.j.olkiewicz@gmail.com>
2019-10-26 13:43:41 +0200

    Working on movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private float speed;
+
+    public bool stop;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!stop)
+        {
+            transform.Translate(Time.deltaTime * speed * Vector3.forward);
+        }
     }
 }
---
+++ Assets/Stop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.transform.position);
+            Debug.Log(transform.position);
+            if (other.transform.position.z >= Mathf.Abs(transform.position.z))
+            {
+                other.transform.position = transform.position;
+                other.GetComponent<Movement>().stop = true;
+            }
+        }
+    }
+}
