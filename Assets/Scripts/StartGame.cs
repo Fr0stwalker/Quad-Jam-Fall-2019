b194d37bd67b7a266f51d79b51508b918464feaf
Ikuti <m.j.olkiewicz@gmail.com>
2019-10-27 22:17:11 +0100

    Removed time blocking at start

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -6,14 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    private void Awake()
-    {
-        Time.timeScale = 0f;
-    }
 
     public void OnStart()
     {
-        Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
 }
