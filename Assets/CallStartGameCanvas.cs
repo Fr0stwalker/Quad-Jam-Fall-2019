01d81e91437d73db03aca434a8ccc73bd7f5d390
Ikuti <m.j.olkiewicz@gmail.com>
2019-10-27 19:16:31 +0100

    Intro hooked up

---
+++ Assets/CallStartGameCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallStartGameCanvas : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+
+    public void StartGameCanvas()
+    {
+        canvas.SetActive(true);
+    }
+}
